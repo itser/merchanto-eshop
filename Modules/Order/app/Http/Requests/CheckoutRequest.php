d5d4ac205c52e83b148abf7c0450c2ee17088675
oleksandr kulchytskyi <itser1106@gmail.com>
2026-06-10 12:16:15 +0300

    Add Form Request rule and CheckoutRequest class

---
+++ Modules/Order/app/Http/Requests/CheckoutRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Order\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckoutRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, list<string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'customer_name' => ['required', 'string', 'max:255'],
+            'customer_email' => ['required', 'email', 'max:255'],
+        ];
+    }
+}
